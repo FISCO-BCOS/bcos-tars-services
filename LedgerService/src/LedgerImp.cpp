1379a55032308027d78a5be6477f241fa326e372
morebtcg <651932351@qq.com>
2021-04-14 18:43:05 +0800

    add tars files

---
+++ LedgerService/src/LedgerImp.cpp
@@ -0,0 +1,20 @@
+﻿#include "LedgerImp.h"
+#include "servant/Application.h"
+
+//////////////////////////////////////////////////////
+void LedgerImp::initialize()
+{
+    //initialize servant here:
+    //...
+}
+
+//////////////////////////////////////////////////////
+void LedgerImp::destroy()
+{
+    //destroy servant here:
+    //...
+}
+
+void LedgerImp::commitBlock(tars::Int32 blockNumber,tars::TarsCurrentPtr current) {
+    
+}
---
+++ LedgerService/src/LedgerImp.h
@@ -0,0 +1,32 @@
+﻿#ifndef _LedgerImp_H_
+#define _LedgerImp_H_
+
+#include "servant/Application.h"
+#include "Ledger.h"
+
+/**
+ *
+ *
+ */
+class LedgerImp : public bcos::Ledger
+{
+public:
+    /**
+     *
+     */
+    virtual ~LedgerImp() override {}
+
+    /**
+     *
+     */
+    virtual void initialize() override;
+
+    /**
+     *
+     */
+    virtual void destroy() override;
+
+    virtual void commitBlock(tars::Int32 blockNumber,tars::TarsCurrentPtr current) override;
+};
+/////////////////////////////////////////////////////
+#endif
