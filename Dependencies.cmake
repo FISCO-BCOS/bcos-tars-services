cee8f44351d243c5283bd071351ad818f151c41e
morebtcg <651932351@qq.com>
2021-05-20 15:49:09 +0800

    add patchTars

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -13,6 +13,14 @@ FetchContent_MakeAvailable(bcos-cmake-scripts)
 list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_BINARY_DIR}/bcos-cmake-scripts)
 
 include(ExternalProject)
+ExternalProject_Add(tarscpp
+    GIT_REPOSITORY https://${GIT_URL_BASE}/TarsCloud/TarsCpp.git
+    GIT_TAG release/2.4
+    SOURCE_DIR ${DEPENDENCIES_DIR}/tarscpp
+    CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=${DEPENDENCIES_DIR}/tarscpp-install
+    PATCH_COMMAND bash ${CMAKE_CURRENT_SOURCE_DIR}/patchTars.sh ${DEPENDENCIES_DIR}/tarscpp ${DEPENDENCIES_DIR}/tarscpp-install
+)
+
 foreach(BCOS_MODULE framework storage front pbft txpool)
     ExternalProject_Add(bcos-${BCOS_MODULE}
         GIT_REPOSITORY https://${GIT_URL_BASE}/FISCO-BCOS/bcos-${BCOS_MODULE}.git
