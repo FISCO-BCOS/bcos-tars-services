84dbb49f95c500da95e603d35049899a385b72fa
cyjseagull <yujiechen_hust@163.com>
2021-06-29 13:13:26 +0800

    add PBFTService implementation

--- DispatcherService/DispatcherServiceClient.h
+++ DispatcherService/DispatcherServiceClient.h
@@ -1,90 +1,129 @@
 #pragma once
 
-#include "DispatcherService.h"
-#include "bcos-framework/interfaces/dispatcher/DispatcherInterface.h"
+#include "../Common/ErrorConverter.h"
 #include "../protocols/BlockHeaderImpl.h"
 #include "../protocols/BlockImpl.h"
-#include "../Common/ErrorConverter.h"
+#include "DispatcherService.h"
+#include "bcos-framework/interfaces/dispatcher/DispatcherInterface.h"
 #include <memory>
 
-namespace bcostars {
-class DispatcherServiceClient : public bcos::dispatcher::DispatcherInterface {
+namespace bcostars
+{
+class DispatcherServiceClient : public bcos::dispatcher::DispatcherInterface
+{
 public:
-  ~DispatcherServiceClient() override {}
+    DispatcherServiceClient(bcostars::DispatcherServicePrx _proxy) : m_proxy(_proxy) {}
+    ~DispatcherServiceClient() override {}
 
-  void asyncExecuteBlock(const bcos::protocol::Block::Ptr &_block, bool _verify,
-                         std::function<void(const bcos::Error::Ptr &, const bcos::protocol::BlockHeader::Ptr &)> _callback) override {
-    class Callback : public bcostars::DispatcherServicePrxCallback {
-    public:
-      Callback(std::function<void(const bcos::Error::Ptr &, const bcos::protocol::BlockHeader::Ptr &)> callback,
-               bcostars::protocol::BlockHeaderFactoryImpl::Ptr blockHeaderFactory)
-          : m_callback(callback), m_blockHeaderFactory(blockHeaderFactory) {}
+    void asyncExecuteBlock(const bcos::protocol::Block::Ptr& _block, bool _verify,
+        std::function<void(const bcos::Error::Ptr&, const bcos::protocol::BlockHeader::Ptr&)>
+            _callback) override
+    {
+        class Callback : public bcostars::DispatcherServicePrxCallback
+        {
+        public:
+            Callback(std::function<void(
+                         const bcos::Error::Ptr&, const bcos::protocol::BlockHeader::Ptr&)>
+                         callback,
+                bcostars::protocol::BlockHeaderFactoryImpl::Ptr blockHeaderFactory)
+              : m_callback(callback), m_blockHeaderFactory(blockHeaderFactory)
+            {}
 
-      void callback_asyncExecuteBlock(const bcostars::Error &ret, const bcostars::BlockHeader &blockHeader) override {
-        auto bcosBlockHeader = m_blockHeaderFactory->createBlockHeader();
-        std::dynamic_pointer_cast<bcostars::protocol::BlockHeaderImpl>(bcosBlockHeader)->setInner(blockHeader);
+            void callback_asyncExecuteBlock(
+                const bcostars::Error& ret, const bcostars::BlockHeader& blockHeader) override
+            {
+                auto bcosBlockHeader = m_blockHeaderFactory->createBlockHeader();
+                std::dynamic_pointer_cast<bcostars::protocol::BlockHeaderImpl>(bcosBlockHeader)
+                    ->setInner(blockHeader);
 
-        m_callback(toBcosError(ret), bcosBlockHeader);
-      }
-      void callback_asyncExecuteBlock_exception(tars::Int32 ret) override { m_callback(toBcosError(ret), nullptr); }
+                m_callback(toBcosError(ret), bcosBlockHeader);
+            }
+            void callback_asyncExecuteBlock_exception(tars::Int32 ret) override
+            {
+                m_callback(toBcosError(ret), nullptr);
+            }
 
-    private:
-      std::function<void(const bcos::Error::Ptr &, const bcos::protocol::BlockHeader::Ptr &)> m_callback;
-      bcostars::protocol::BlockHeaderFactoryImpl::Ptr m_blockHeaderFactory;
-    };
+        private:
+            std::function<void(const bcos::Error::Ptr&, const bcos::protocol::BlockHeader::Ptr&)>
+                m_callback;
+            bcostars::protocol::BlockHeaderFactoryImpl::Ptr m_blockHeaderFactory;
+        };
 
-    m_proxy->async_asyncExecuteBlock(new Callback(_callback, m_blockHeaderFactory), std::dynamic_pointer_cast<bcostars::protocol::BlockImpl>(_block)->inner(),
-                                     _verify);
-  }
+        m_proxy->async_asyncExecuteBlock(new Callback(_callback, m_blockHeaderFactory),
+            std::dynamic_pointer_cast<bcostars::protocol::BlockImpl>(_block)->inner(), _verify);
+    }
 
-  void asyncGetLatestBlock(std::function<void(const bcos::Error::Ptr &, const bcos::protocol::Block::Ptr &)> _callback) override {
-    class Callback : public bcostars::DispatcherServicePrxCallback {
-    public:
-      Callback(std::function<void(const bcos::Error::Ptr &, const bcos::protocol::Block::Ptr &)> callback,
-               bcostars::protocol::BlockFactoryImpl::Ptr blockFactory)
-          : m_callback(callback), m_blockFactory(blockFactory) {}
+    void asyncGetLatestBlock(
+        std::function<void(const bcos::Error::Ptr&, const bcos::protocol::Block::Ptr&)> _callback)
+        override
+    {
+        class Callback : public bcostars::DispatcherServicePrxCallback
+        {
+        public:
+            Callback(std::function<void(const bcos::Error::Ptr&, const bcos::protocol::Block::Ptr&)>
+                         callback,
+                bcostars::protocol::BlockFactoryImpl::Ptr blockFactory)
+              : m_callback(callback), m_blockFactory(blockFactory)
+            {}
 
-      void callback_asyncGetLatestBlock(const bcostars::Error &ret, const bcostars::Block &block) override {
-        auto bcosBlock = m_blockFactory->createBlock();
-        std::dynamic_pointer_cast<bcostars::protocol::BlockImpl>(bcosBlock)->setInner(block);
+            void callback_asyncGetLatestBlock(
+                const bcostars::Error& ret, const bcostars::Block& block) override
+            {
+                auto bcosBlock = m_blockFactory->createBlock();
+                std::dynamic_pointer_cast<bcostars::protocol::BlockImpl>(bcosBlock)->setInner(
+                    block);
 
-        m_callback(toBcosError(ret), bcosBlock);
-      }
+                m_callback(toBcosError(ret), bcosBlock);
+            }
 
-      void callback_asyncGetLatestBlock_exception(tars::Int32 ret) override { m_callback(toBcosError(ret), nullptr); }
+            void callback_asyncGetLatestBlock_exception(tars::Int32 ret) override
+            {
+                m_callback(toBcosError(ret), nullptr);
+            }
 
-    private:
-      std::function<void(const bcos::Error::Ptr &, const bcos::protocol::Block::Ptr &)> m_callback;
-      bcostars::protocol::BlockFactoryImpl::Ptr m_blockFactory;
-    };
+        private:
+            std::function<void(const bcos::Error::Ptr&, const bcos::protocol::Block::Ptr&)>
+                m_callback;
+            bcostars::protocol::BlockFactoryImpl::Ptr m_blockFactory;
+        };
 
-    m_proxy->async_asyncGetLatestBlock(new Callback(_callback, m_blockFactory));
-  }
+        m_proxy->async_asyncGetLatestBlock(new Callback(_callback, m_blockFactory));
+    }
 
-  void asyncNotifyExecutionResult(const bcos::Error::Ptr &_error, const bcos::protocol::BlockHeader::Ptr &_header,
-                                  std::function<void(const bcos::Error::Ptr &)> _callback) override {
-    class Callback : public bcostars::DispatcherServicePrxCallback {
-    public:
-      Callback(std::function<void(const bcos::Error::Ptr &)> callback) : m_callback(callback) {}
+    void asyncNotifyExecutionResult(const bcos::Error::Ptr& _error,
+        const bcos::protocol::BlockHeader::Ptr& _header,
+        std::function<void(const bcos::Error::Ptr&)> _callback) override
+    {
+        class Callback : public bcostars::DispatcherServicePrxCallback
+        {
+        public:
+            Callback(std::function<void(const bcos::Error::Ptr&)> callback) : m_callback(callback)
+            {}
 
-      void callback_asyncNotifyExecutionResult(const bcostars::Error &ret) override { m_callback(toBcosError(ret)); }
+            void callback_asyncNotifyExecutionResult(const bcostars::Error& ret) override
+            {
+                m_callback(toBcosError(ret));
+            }
 
-      void callback_asyncNotifyExecutionResult_exception(tars::Int32 ret) override { m_callback(toBcosError(ret)); }
+            void callback_asyncNotifyExecutionResult_exception(tars::Int32 ret) override
+            {
+                m_callback(toBcosError(ret));
+            }
 
-    private:
-      std::function<void(const bcos::Error::Ptr &)> m_callback;
-    };
+        private:
+            std::function<void(const bcos::Error::Ptr&)> m_callback;
+        };
 
-    m_proxy->async_asyncNotifyExecutionResult(new Callback(_callback), toTarsError(_error),
-                                              std::dynamic_pointer_cast<bcostars::protocol::BlockHeaderImpl>(_header)->inner());
-  }
+        m_proxy->async_asyncNotifyExecutionResult(new Callback(_callback), toTarsError(_error),
+            std::dynamic_pointer_cast<bcostars::protocol::BlockHeaderImpl>(_header)->inner());
+    }
 
-  void start() override {}
-  void stop() override {}
+    void start() override {}
+    void stop() override {}
 
 private:
-  bcostars::DispatcherServicePrx m_proxy;
-  bcostars::protocol::BlockHeaderFactoryImpl::Ptr m_blockHeaderFactory;
-  bcostars::protocol::BlockFactoryImpl::Ptr m_blockFactory;
+    bcostars::DispatcherServicePrx m_proxy;
+    bcostars::protocol::BlockHeaderFactoryImpl::Ptr m_blockHeaderFactory;
+    bcostars::protocol::BlockFactoryImpl::Ptr m_blockFactory;
 };
-} // namespace bcostars
+}  // namespace bcostars
--- PBFTService/PBFTServiceClient.cpp
+++ PBFTService/PBFTServiceClient.cpp
@@ -1,1 +1,115 @@
+/**
+ *  Copyright (C) 2021 FISCO BCOS.
+ *  SPDX-License-Identifier: Apache-2.0
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ * @brief client for the PBFTService
+ * @file PBFTServiceClient.cpp
+ * @author: yujiechen
+ * @date 2021-06-29
+ */
+
 #include "PBFTServiceClient.h"
+#include "../protocols/BlockImpl.h"
+#include "Common.h"
+
+using namespace bcostars;
+
+void PBFTServiceClient::asyncSubmitProposal(bcos::bytesConstRef _proposalData,
+    bcos::protocol::BlockNumber _proposalIndex, bcos::crypto::HashType const& _proposalHash,
+    std::function<void(bcos::Error::Ptr)> _onProposalSubmitted)
+{
+    m_proxy->async_asyncSubmitProposal(new PBFTServiceCommonCallback(_onProposalSubmitted),
+        _proposalData.toBytes(), _proposalIndex, _proposalHash.asBytes());
+}
+
+void PBFTServiceClient::asyncGetPBFTView(
+    std::function<void(bcos::Error::Ptr, bcos::consensus::ViewType)> _onGetView)
+{
+    class Callback : public PBFTServicePrxCallback
+    {
+    public:
+        explicit Callback(
+            std::function<void(bcos::Error::Ptr, bcos::consensus::ViewType)> _callback)
+          : PBFTServicePrxCallback(), m_callback(_callback)
+        {}
+        ~Callback() override {}
+
+        void callback_asyncGetPBFTView(const bcostars::Error& ret, tars::Int64 _view) override
+        {
+            m_callback(toBcosError(ret), _view);
+        }
+        void callback_asyncGetPBFTView_exception(tars::Int32 ret) override
+        {
+            m_callback(toBcosError(ret), 0);
+        }
+
+    private:
+        std::function<void(bcos::Error::Ptr, bcos::consensus::ViewType)> m_callback;
+    };
+    m_proxy->async_asyncGetPBFTView(new Callback(_onGetView));
+}
+
+// the sync module calls this interface to check block
+void PBFTServiceClient::asyncCheckBlock(
+    bcos::protocol::Block::Ptr _block, std::function<void(bcos::Error::Ptr, bool)> _onVerifyFinish)
+{
+    class Callback : public PBFTServicePrxCallback
+    {
+    public:
+        explicit Callback(std::function<void(bcos::Error::Ptr, bool)> _callback)
+          : PBFTServicePrxCallback(), m_callback(_callback)
+        {}
+        ~Callback() override {}
+
+        void callback_asyncCheckBlock(const bcostars::Error& ret, tars::Bool _verifyResult) override
+        {
+            m_callback(toBcosError(ret), _verifyResult);
+        }
+        void callback_asyncCheckBlock_exception(tars::Int32 ret) override
+        {
+            m_callback(toBcosError(ret), false);
+        }
+
+    private:
+        std::function<void(bcos::Error::Ptr, bool)> m_callback;
+    };
+
+    auto blockImpl = std::dynamic_pointer_cast<bcostars::protocol::BlockImpl>(_block);
+    m_proxy->async_asyncCheckBlock(new Callback(_onVerifyFinish), blockImpl->inner());
+}
+
+// the sync module calls this interface to notify new block
+void PBFTServiceClient::asyncNotifyNewBlock(
+    bcos::ledger::LedgerConfig::Ptr _ledgerConfig, std::function<void(bcos::Error::Ptr)> _onRecv)
+{
+    m_proxy->async_asyncNotifyNewBlock(
+        new PBFTServiceCommonCallback(_onRecv), toTarsLedgerConfig(_ledgerConfig));
+}
+
+// called by frontService to dispatch message
+void PBFTServiceClient::asyncNotifyConsensusMessage(bcos::Error::Ptr _error,
+    std::string const& _uuid, bcos::crypto::NodeIDPtr _nodeID, bcos::bytesConstRef _data,
+    std::function<void(bcos::Error::Ptr _error)> _onRecv)
+{
+    m_proxy->async_asyncNotifyConsensusMessage(
+        new PBFTServiceCommonCallback(_onRecv), _uuid, _nodeID->data(), _data.toBytes());
+}
+
+// Note: used for the txpool notify the unsealed txsSize
+void PBFTServiceClient::asyncNoteUnSealedTxsSize(
+    size_t _unsealedTxsSize, std::function<void(bcos::Error::Ptr)> _onRecv)
+{
+    m_proxy->async_asyncNoteUnSealedTxsSize(
+        new PBFTServiceCommonCallback(_onRecv), _unsealedTxsSize);
+}
