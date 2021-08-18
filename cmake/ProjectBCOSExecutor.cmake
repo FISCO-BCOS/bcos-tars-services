bbfd7472a1dd763cee3c98936ead6cb2de26f31f
kyonRay <rui.kwok@outlook.com>
2021-08-18 14:06:49 +0800

    <fix>(cmake): update bcos-executor.

--- cmake/ProjectBCOSExecutor.cmake
+++ cmake/ProjectBCOSExecutor.cmake
@@ -1,7 +1,7 @@
 include(FetchContent)
 FetchContent_Declare(executor_project
     GIT_REPOSITORY https://${URL_BASE}/FISCO-BCOS/bcos-executor.git
-    GIT_TAG        aad7b878f0f9f8b2f0f5414c72e7d67d028b1bb7
+    GIT_TAG        5d79865fe2c578693d09d1f8804e9445ea1457a6
     # SOURCE_DIR     ${CMAKE_SOURCE_DIR}/deps/src/bcos-executor
 )
 
