94cb16d4d2731b2e79adae5ba1df170603fa6b42
cyjseagull <yujiechen_hust@163.com>
2021-11-16 21:20:22 +0800

    update rpc to fix empty transactionHash

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,7 +1,7 @@
 # Note: hunter_config takes effect globally, it is not recommended to set it in bcos-node, otherwise it will affect all projects that rely on bcos-framework
 hunter_config(bcos-framework VERSION 3.0.0-local
-	URL https://${URL_BASE}/FISCO-BCOS/bcos-framework/archive/9f24369f40c378e4ce169983ac5a58d5736fa5d7.tar.gz
-    SHA1 7c75875a0f19505fc61412718fc4304907da26a9
+	URL https://${URL_BASE}/FISCO-BCOS/bcos-framework/archive/1294f1988c112e1df0109d87cb574a704de92bd4.tar.gz
+    SHA1 05c735f032d3535410c4ea31acc8616fb0c60415
 	CMAKE_ARGS HUNTER_PACKAGE_LOG_BUILD=ON HUNTER_PACKAGE_LOG_INSTALL=ON
 )
 
@@ -26,8 +26,8 @@ hunter_config(bcos-tars-protocol
 )
 
 hunter_config(bcos-txpool VERSION 3.0.0-local-beda0a00
-    URL  https://${URL_BASE}/FISCO-BCOS/bcos-txpool/archive/d175ee78263dae9edcd58f45a064b53ac8ff5f1c.tar.gz
-    SHA1 00c794e38f3daa1c0ecd9beb163b751b43e228c4
+    URL  https://${URL_BASE}/FISCO-BCOS/bcos-txpool/archive/bc104bcee00f50062c991f76a6a4150a08a09f52.tar.gz
+    SHA1 fe2f590bdaf8b27166aa59eff7d68baa1f895c9b
     CMAKE_ARGS URL_BASE=${URL_BASE}  HUNTER_KEEP_PACKAGE_SOURCES=ON
 )
 
@@ -82,8 +82,8 @@ hunter_config(bcos-scheduler VERSION 3.0.0-local-b1c450b95b6f83e6a0cf3bd2f385c2d
 )
 
 hunter_config(bcos-rpc VERSION 3.0.0-local
-    URL  https://${URL_BASE}/FISCO-BCOS/bcos-rpc/archive/c5d992064e8dc6d1412feef6b5073e7c7930724d.tar.gz
-    SHA1 5aba3fea863f39516df6749fba1314badf15eaf6
+    URL  https://${URL_BASE}/FISCO-BCOS/bcos-rpc/archive/821e8f97d94abf5e8216257c9315940da4a73f12.tar.gz
+    SHA1 9e9d1e096b36bcfe62b33be35bbda378df33eebe
     CMAKE_ARGS URL_BASE=${URL_BASE} HUNTER_KEEP_PACKAGE_SOURCES=ON
 )
 
